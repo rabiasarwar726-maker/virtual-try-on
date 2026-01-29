63f85f4e03b58bcd90717226b7feebd1a9b5219b
rabiasarwar726-maker <rsarwar726@gmail.com>
2026-01-29 23:46:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
-from mediapipe as mp
+from mediapipe import as mp
 
 # -------------------------------
 # Page Configuration
@@ -85,5 +85,6 @@ if person_file and garment_file:
 
     # Display final image
     st.image(person_img, channels="RGB", caption="Virtual Try-On Result")
+
 
 
